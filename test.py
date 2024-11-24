73564f5a0244e0a7fe42e2f881c7586d22537b33
BIGSMATER <1025076763@qq.com>
2024-11-24 22:18:58 +0800

    add Readme & greater code

--- test.py
+++ test.py
@@ -1,19 +1,12 @@
 import json
-import socket
-import traceback
-import pandas as pd
-import requests
 import os
 from selenium import webdriver
 
 #refs:
 #https://blog.csdn.net/Demonslzh/article/details/125062240
+#https://www.cnblogs.com/superhin/p/12600358.html
 
-# chrome data path
-# hostname = socket.gethostname()  #暂时有bug
-# CHROME_PATH = f"C:/Users/{hostname}/AppData/Local/Google/Chrome/User Data/Default"
-# EDGE_PATH = f"C:/Users/{hostname}/AppData/Local/Microsoft/Edge/User Data/Default"
-hostname = "xxx"#your username
+hostname=os.getlogin()
 CHROME_PATH = f"C:/Users/{hostname}/AppData/Local/Google/Chrome/User Data/Default"
 EDGE_PATH = f"C:/Users/{hostname}/AppData/Local/Microsoft/Edge/User Data/Default"
 EDGE_KILL = "taskkill /f /t /im msedge.exe"
@@ -22,26 +15,15 @@ print(EDGE_PATH)
 
 class BookMark:
 
-    def __init__(self, chromePath=EDGE_PATH):
+    def __init__(self, Path=EDGE_PATH):
         # chromepath
-        self.chromePath = chromePath
+        self.Path = Path
         # refresh bookmarks
         self.bookmarks = self.get_bookmarks()
         self.driver = webdriver.Edge()
-    # def get_folder_data(self):
-    #     """获取收藏夹所有的文件夹内容，合并后保存"""
-    #     df = []
-    #     for mark_name, item in self.bookmarks["roots"].items():
-    #         try:
-    #             data = pd.DataFrame(item["children"])
-    #             data["folder_name"] =item["name"]
-    #             df.append(data)
-    #         except Exception:
-    #             traceback.print_exc()
-    #             print(mark_name)
-    #     pd.concat(df).to_csv("results.csv",encoding="utf-8")
+
     def get_folder_data(self):
-        self.creatfolder(r"./本地收藏夹")
+        self.creatfolder(r"./本地收藏夹") #可以改成绝对路径
         for mark_name, item in self.bookmarks["roots"].items():
             self.dfs(item,r"./本地收藏夹")
 
@@ -49,9 +31,9 @@ class BookMark:
         'update chrome data from chrome path'
         # parse bookmarks
         assert os.path.exists(
-            os.path.join(self.chromePath,
+            os.path.join(self.Path,
                          'Bookmarks')), "can't found ‘Bookmarks’ file,or path isn't a chrome browser cache path!"
-        with open(os.path.join(self.chromePath, 'Bookmarks'), encoding='utf-8') as f:
+        with open(os.path.join(self.Path, 'Bookmarks'), encoding='utf-8') as f:
             return json.loads(f.read())
 
     def creatfolder(self,path):
@@ -70,8 +52,8 @@ class BookMark:
         except Exception:
             print(Exception)
         # 2. 写入文件
-        e="bug"
-        with open((r"{}".format(path+".mhtml")), 'w', newline='') as f:#吾爱破解 - LCG - LSG|安卓破解|病毒分析|www.52pojie.cn.mhtml 会报错，研究许久发现文件名不能有|
+        with open((r"{}".format(path+".mhtml")), 'w', newline='') as f:#吾爱破解 - LCG - LSG|安卓破解|病毒分析|www.52pojie.cn.mhtml 会报错，
+                                                                       #研究许久发现文件名不能有|以及其他一些符号，具体自己找个文件重命名试一下即可，会有提示的
             f.write(res['data'])
 
     def dfs(self,root,path):
@@ -81,12 +63,7 @@ class BookMark:
         elif root["type"]=="folder":
             self.creatfolder(path+'/'+root["name"].translate(str.maketrans({'/':e,'\\':e,'|':e,':':e,'*':e,'?':e,'"':e,'<':e,'>':e})))
             for i in root["children"]:
-                # for markname, item in i.items():
                     self.dfs(i,r"{}".format(path+'/'+root["name"].translate(str.maketrans({'/':e,'\\':e,'|':e,':':e,'*':e,'?':e,'"':e,'<':e,'>':e}))))
-            # if item["type"]=="url":
-            #     self.creatmhtml(path+'/'+item["name"],item["url"])
-            # elif item["type"]=="folder":
-            #     self.creatfolder(path+'/'+item["name"])
 
     
 test=BookMark()
